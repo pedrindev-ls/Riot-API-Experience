0bb8b7fad928ec4f09fd74f6b41dd26950fad085
MauroMarchesan88 <mauromarchesan88@gmail.com>
2022-02-11 18:12:22 -0300

    Merge branch 'master' of github.com:pedrindev-ls/Riot-API-Experience

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-window.onload = () => {};
+const splash = document.getElementById('champion-image');
+const champName = document.getElementById('title-name');
+// const nameTitle = document.createElement('p');
+const spells = document.createElement('ol');
+const passive = document.createElement('div');
+
+const url = 'http://ddragon.leagueoflegends.com/cdn/12.3.1/data/en_US/champion/Malzahar.json'
+// nameTitle.innerText = url.data;
+console.log(champName);
+console.log(splash);
+// champName.appendChild(nameTitle);
+
+// const body = document.querySelector('body');
+
+
+// window.onload = () => {};
