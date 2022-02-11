b62af4e9a284f5664501821fbabfed73f5f396ff
MauroMarchesan88 <mauromarchesan88@gmail.com>
2022-02-11 19:46:40 -0300

    resolvida a logica do fetch porem e preciso melhorar a sintaxe para admitir outrs inputs corretamente

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const splash = document.getElementById('champion-image');
-const champName = document.getElementById('title-name');
-// const nameTitle = document.createElement('p');
+const champName = document.querySelector('.title-name');
+const nameTitle = document.createElement('p');
 const spells = document.createElement('ol');
 const passive = document.createElement('div');
-
-const url = 'http://ddragon.leagueoflegends.com/cdn/12.3.1/data/en_US/champion/Malzahar.json'
-// nameTitle.innerText = url.data;
-console.log(champName);
-console.log(splash);
-// champName.appendChild(nameTitle);
-
+const chosenChamp = document.getElementById('search');
+const inputButton = document.getElementById('inputButton')
+const url = `http://ddragon.leagueoflegends.com/cdn/12.3.1/data/en_US/champion/${chosenChamp.value}.json`
+const fetchChamp = async () => {
+    console.log('run')
+    fetch(url)
+    .then(response => response.json())
+    .then(data => nameTitle.innerText = `data.data.${chosenChamp.value}.id data.data.${chosenChamp.value}.title`)
+};
+champName.appendChild(nameTitle)
+inputButton.addEventListener('click', fetchChamp)
 // const body = document.querySelector('body');
 
 
