203330e46e58dd95872cdc0b74f29bb61d9089f2
MauroMarchesan88 <mauromarchesan88@gmail.com>
2022-02-12 09:09:38 -0300

    Resolvida sintaxe da funcao fetchChamp para o id e title

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ const spells = document.createElement('ol');
 const passive = document.createElement('div');
 const chosenChamp = document.getElementById('search');
 const inputButton = document.getElementById('inputButton')
-const url = `http://ddragon.leagueoflegends.com/cdn/12.3.1/data/en_US/champion/${chosenChamp.value}.json`
 const fetchChamp = async () => {
-    console.log('run')
+    console.log(chosenChamp.value)
+    const chosenOne = chosenChamp.value;
+    const url = `http://ddragon.leagueoflegends.com/cdn/12.3.1/data/en_US/champion/${chosenChamp.value}.json`
     fetch(url)
     .then(response => response.json())
-    .then(data => nameTitle.innerText = `data.data.${chosenChamp.value}.id data.data.${chosenChamp.value}.title`)
+    .then(data => nameTitle.innerText = data.data[chosenOne].id + ' ' + data.data[chosenOne].title)
 };
 champName.appendChild(nameTitle)
 inputButton.addEventListener('click', fetchChamp)
