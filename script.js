51f1e3b23cd159416ee6eaa84804e5d069a96726
MauroMarchesan88 <mauromarchesan88@gmail.com>
2022-02-12 14:26:22 -0300

    Sintaxe melhorada para separar funcao de dataFetch para pegar toda a data e depois distribuir por funcoes em cada parte do DOM a preencher

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+import { fetchChamp } from "./fetchChamp.js";
+
 const splash = document.getElementById('champion-image');
 const champName = document.querySelector('.title-name');
 const nameTitle = document.createElement('p');
 const spells = document.createElement('ol');
 const passive = document.createElement('div');
-const chosenChamp = document.getElementById('search');
 const inputButton = document.getElementById('inputButton')
-const fetchChamp = async () => {
-    console.log(chosenChamp.value)
-    const chosenOne = chosenChamp.value;
-    const url = `http://ddragon.leagueoflegends.com/cdn/12.3.1/data/en_US/champion/${chosenChamp.value}.json`
-    fetch(url)
-    .then(response => response.json())
-    .then(data => nameTitle.innerText = data.data[chosenOne].id + ' ' + data.data[chosenOne].title)
-};
-champName.appendChild(nameTitle)
-inputButton.addEventListener('click', fetchChamp)
-// const body = document.querySelector('body');
+const inputChamp = document.getElementById('search');
 
+const idTitleFetch = async (rawData) => {
+    const chosenOne = inputChamp.value;
+    const id = rawData.data[chosenOne].id
+    const title = rawData.data[chosenOne].title
+    nameTitle.innerText = `${id} ${title}`
+    champName.appendChild(nameTitle);
+}
+
+const dataFetch = async () => {
+    const rawData = await fetchChamp();
+    idTitleFetch(rawData);
+
+};
+inputButton.addEventListener('click', dataFetch);
 
 // window.onload = () => {};
