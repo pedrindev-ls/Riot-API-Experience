109cac11e5a6d3f28f897b89c5bc54c3720bc727
MauroMarchesan88 <mauromarchesan88@gmail.com>
2022-02-12 15:00:34 -0300

    Lore do Champ, disponivel

--- script.js
+++ script.js
@@ -3,28 +3,37 @@ import { fetchChamp } from "./fetchChamp.js";
 const splash = document.getElementById('champion-image');
 const champName = document.querySelector('.title-name');
 const nameTitle = document.createElement('p');
-const inputButton = document.getElementById('inputButton')
+const champLore = document.getElementById('lore');
+const inputButton = document.getElementById('inputButton');
 const inputChamp = document.getElementById('search');
 
 const idTitleFetch = async (rawData) => {
     const chosenOne = inputChamp.value;
-    const id = rawData.data[chosenOne].id
-    const title = rawData.data[chosenOne].title
-    nameTitle.innerText = `${id} ${title}`
+    const id = rawData.data[chosenOne].id;
+    const title = rawData.data[chosenOne].title;
+    nameTitle.innerText = `${id} ${title}`;
     champName.appendChild(nameTitle);
 }
-const splashFetch = (rawData) => {
+const splashFetch = () => {
     const chosenOne = inputChamp.value;
     splash.innerHTML = '';
     const splashImg = document.createElement('img');
-    const splashChosen = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${chosenOne}_0.jpg`
+    const splashChosen = `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${chosenOne}_0.jpg`;
     splashImg.src = splashChosen;
     splash.append(splashImg);
+}
+const loreFetch = (rawData) => {
+    const chosenOne = inputChamp.value;
+    champLore.innerHTML = '';
+    const lore = document.createElement('p');
+    lore.innerText = rawData.data[chosenOne].lore;
+    champLore.appendChild(lore);
 }
 const dataFetch = async () => {
     const rawData = await fetchChamp();
     idTitleFetch(rawData);
-    splashFetch(rawData);
+    splashFetch();
+    loreFetch(rawData)
 
 };
 inputButton.addEventListener('click', dataFetch);
